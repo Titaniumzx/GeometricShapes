ced9712a929bb0afe0f10c0fe83cc3279df1c870
Victor Vladimirovich <TITANIUMZX@MAIL.RU>
2025-05-19 16:16:27 +0300

    Test project added

---
+++ TestGeometricShapes/CircleTests.cs
@@ -0,0 +1,25 @@
+﻿using GeometricShapes;
+
+namespace TestGeometricShapes
+{
+    [TestFixture]
+    public class CircleTests
+    {
+        [TestCase(67, 14102.60942)]
+        [TestCase(1, 3.1416)]
+        [TestCase(3.9, 47.78362)]
+        [TestCase(0, 0)]
+        public void CalculateAreaValidRadiusReturnsCorrectArea(double radius, double expected)
+        {
+            IShape circle = new Circle(radius);
+            Assert.That(circle.CalculateArea(), Is.EqualTo(expected).Within(0.0001));
+        }
+
+        [TestCase(-1)]
+        [TestCase(-0.03)]
+        public void ConstructorInvalidRadiusThrowsException(double radius)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Circle(radius));
+        }
+    }
+}
---
+++ TestGeometricShapes/TriangleTests.cs
@@ -0,0 +1,48 @@
+﻿using GeometricShapes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGeometricShapes
+{
+    [TestFixture]
+    internal class TriangleTests
+    {     
+        [TestCase(-1, 1, 1)]
+        [TestCase(1, -1, 1)]
+        [TestCase(1, 1, -1)]
+        [TestCase(1, 2, 4)] 
+        public void ConstructorInvalidSidesThrowsException(double a, double b, double c)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Triangle(a, b, c));
+        }
+       
+        [TestCase(3, 4, 5, 6)]
+        [TestCase(5, 5, 6, 12)]
+        [TestCase(7, 8, 9, 26.8328)]
+        public void CalculateAreaValidTriangleReturnsCorrectArea(double a, double b, double c, double expected)
+        {
+            IShape triangle = new Triangle(a, b, c);
+            Assert.That(triangle.CalculateArea(), Is.EqualTo(expected).Within(0.0001));
+        }
+       
+        [TestCase(3, 4, 5)]
+        [TestCase(5, 12, 13)]
+        [TestCase(8, 15, 17)]
+        public void CalculateAreaRightTriangleUsesRightFormula(double a, double b, double c)
+        {
+            IShape triangle = new Triangle(a, b, c);
+            double expected = (a * b) / 2;
+            Assert.That(triangle.CalculateArea(), Is.EqualTo(expected).Within(0.0001));
+        }
+    
+        [Test]
+        public void CalculateAreaEquilateralTriangleReturnsCorrectArea()
+        {
+            IShape triangle = new Triangle(2, 2, 2);
+            Assert.That(triangle.CalculateArea(), Is.EqualTo(1.73205).Within(0.0001));
+        }
+    }
+}
