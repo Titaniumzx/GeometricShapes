fcb1cbee54fc6762470bc5267e24a5278e8abce9
Victor Vladimirovich <TITANIUMZX@MAIL.RU>
2025-05-19 13:43:39 +0300

    Test project added

---
+++ TestGeometricShapes/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace TestGeometricShapes
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
